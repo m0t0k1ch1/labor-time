e57c0b8801a5ccd1be9a19fee4a5bd7d6a8c2aab
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2014-02-25 19:20:16 +0900

    for debug

--- lib/LaborTime/Web/Dispatcher.pm
+++ lib/LaborTime/Web/Dispatcher.pm
@@ -243,6 +243,10 @@ sub work_logs {
                 if ($segment->{place}->{id} == $workplace_id) {
                     my $start_time = DateTime::Format::HTTP->parse_datetime($segment->{startTime});
                     my $end_time   = DateTime::Format::HTTP->parse_datetime($segment->{endTime});
+
+                    warn $start_time->ymd;
+                    warn $result->{date};
+
                     if ($start_time->ymd eq $result->{date}) {
                         push @time_logs, {
                             start_time => $start_time->hms,
